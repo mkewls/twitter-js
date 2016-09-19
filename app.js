53c25637e463580abea28b8522bfb034326b29cf
Kenty Wang <kentymail@gmail.com>
2016-09-19 18:00:56 -0400

    new commit

--- app.js
+++ app.js
@@ -2,12 +2,22 @@
 
 var express = require( 'express' )
 var morgan = require( 'morgan' )
+var nunjucks = require('nunjucks')
 var app = express()
+
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+
+nunjucks.configure('views', { noCache: true })
+
+
 
 app.use(morgan('dev'))
 
 app.get('/', function(req, res) {
-  res.send('Hey yooo!')
+	var people = [{name: '1'}, {name: '2'}, {name: '3'}];
+	res.render( 'index', {title: 'Hello', people: people} );
 })
 
 app.listen(8080);
