b02583b9226374d775f29d4c6fcee87282da92b6
mkewls <mw@mikewill.net>
2016-09-20 11:01:57 -0400

    updated router, rendering

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 'use strict'
 
-var express = require( 'express' )
-var morgan = require( 'morgan' )
-var nunjucks = require('nunjucks')
-var app = express()
+var express = require( 'express' );
+var morgan = require( 'morgan' );
+var nunjucks = require('nunjucks');
+var routes = require('./routes');
+var app = express();
 
-nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+nunjucks.configure('views', { noCache: true })
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
-nunjucks.configure('views', { noCache: true })
-
-
-
-app.use(morgan('dev'))
-
-app.get('/', function(req, res) {
-	var people = [{name: '1'}, {name: '2'}, {name: '3'}];
-	res.render( 'index', {title: 'Hello', people: people} );
-})
+app.use(morgan('dev'));
+app.use('/', routes);
+app.use(express.static('public'));
 
 app.listen(8080);
