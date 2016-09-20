224e6f1772eb12f4fbd560410a69878b0d837f93
mkewls <mw@mikewill.net>
2016-09-20 14:24:13 -0400

    updated with forms, and socket.io

--- app.js
+++ app.js
@@ -3,16 +3,16 @@
 var express = require( 'express' );
 var morgan = require( 'morgan' );
 var nunjucks = require('nunjucks');
+var socketio = require('socket.io');
 var routes = require('./routes');
 var app = express();
+var server = app.listen(8080);
+var io = socketio.listen(server);
 
 nunjucks.configure('views', { noCache: true })
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
 app.use(morgan('dev'));
-app.use('/', routes);
+app.use('/', routes(io));
 app.use(express.static('public'));
-
-app.listen(8080);
-//
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,43 @@
+'use strict'
+
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 // could use one line instead: var router = require('express').Router();
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var tweetBank = require('../tweetBank');
 
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-     // console.log('tweets: ', tweets);
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets, showForm: true, showName: false } );
 });
 
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
-
   var tweets = tweetBank.find(function(element){
 	return element.name.indexOf(name) > -1;
-});
-    //console.log('tweets: ', tweets);
-  res.render( 'index', { tweets: tweets } );
+  });
+  res.render( 'index', { tweets: tweets, showForm: true, showName: true } );
 });
 
 router.get('/tweets/:ident', function(req, res) {
   var ident = Number(req.params.ident);
-  console.log(typeof ident);
   var tweets = tweetBank.find({ident: ident});
-    //console.log('tweets: ', tweets);
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets, showForm: false } );
 });
 
+router.post('/tweets', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
+});
 
-module.exports = router;
+// io.sockets.emit('newTweet', { tweets: tweets });
+
+module.exports = function (io) {
+  router.io = io;
+  return router;
+};
